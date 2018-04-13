ff7a0067c6a6d8244c2e97f8a1ef195085686f49
joshuaagapay <jpagapay94@gmail.com>
2018-04-13 23:51:43 +0800

    Third Commit

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,18 +1,19 @@
 const request = require('request');
 
-const findUser = (username) => {
+const findUser = (username, handleResult) => {
 
     const options = {
         url:`https://api.github.com/users/${username}`,
         method: 'GET',
         headers: {
             'User-Agent': 'joshuaagapay'
-        }
+        },
+
     }
 
     request(options, (err, res, body) => {
-        let json = JSON.parse(body);
-        console.info(json);
+       let json = JSON.parse(body);
+        handleResult(json);
     });
 }
 
--- index.js
+++ index.js
@@ -8,14 +8,22 @@ const program  = require('commander');
 
 const findUser = require('./controllers/controller.js');
 
+const find = (username) => {
+    return username.split(",");
+};
+
 program
     .version('1.0.0')
 
 program
-    .command('find <username>')
+    .usage('find <username>')
     .alias('f')
     .description('Find users from github')
-    .action(username => findUser(username));
+    .action(username => findUser(username, (result) => {
+        console.log(result);
+    })
+    );
 
 program.parse(process.argv);
+
 
