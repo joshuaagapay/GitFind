062e67d8dc3ef5c61614bd6dcd2a31191753cf6b
joshuaagapay <jpagapay94@gmail.com>
2018-04-16 22:18:01 +0800

    Added passing test

--- test.js
+++ test.js
@@ -1,27 +1,9 @@
-const findUser = require('./controllers/controller')
+const {findUser} = require('./controllers/controller')
 const assert = require('assert');
-const request = require('request');
 
 describe('demo test', () => {
     it('return specific user', () => {
         const user = 'joshuaagapay';
-
-        const findUser = (username, handleResult) => {
-
-            const options = {
-                url:`https://api.github.com/users/${username}`,
-                method: 'GET',
-                headers: {
-                    'User-Agent': 'joshuaagapay'
-                },
-        
-            }
-        
-            request(options, (err, res, body) => {
-               let json = JSON.parse(body);
-                handleResult(json);
-            });
-        }
 
         findUser(user, (result) => {
             assert(user === result.login);
